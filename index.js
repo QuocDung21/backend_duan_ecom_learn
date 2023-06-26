9a01dcfb111b7bd8188a696e8932c11f76e77b56
Quốc Dũng <91933007+QuocDung21@users.noreply.github.com>
2023-06-26 13:06:00 +0700

    add check

--- index.js
+++ index.js
@@ -25,9 +25,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use("/api/check", (res, req) => {
-  return res.json("Api running");
-});
+
 app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
 app.use("/api/blog", blogRouter);
